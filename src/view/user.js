da43b7d616ebe92d9ab1d937e057ededbef340a5
morwen27 <54504308+morwen27@users.noreply.github.com>
2020-11-19 13:01:59 +0700

    Update user.js

--- src/view/user.js
+++ src/view/user.js
@@ -1,5 +1,5 @@
 export const user = () => {
-    return `<section class="header__profile profile">
+  return `<section class="header__profile profile">
                 <p class="profile__rating">Sci-Fighter</p>
                 <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
             </section>`;
