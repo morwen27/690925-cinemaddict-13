5d1eaf5eb167de327f224e1e6cd76b90b0816c1a
morwen27 <54504308+morwen27@users.noreply.github.com>
2020-11-19 13:01:24 +0700

    Update popup.js

--- src/view/popup.js
+++ src/view/popup.js
@@ -1,5 +1,5 @@
 export const popup = () => {
-    return `<section class="film-details">
+  return `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
       <div class="film-details__top-container">
         <div class="film-details__close">
