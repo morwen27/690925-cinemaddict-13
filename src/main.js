f6d4591f77722c092d84c3200b8dcfb37dac80a7
Крикунова Ольга <shersh.ol@gmail.com>
2020-11-23 00:24:37 +0700

    Карточка фильмачастично попап

--- src/main.js
+++ src/main.js
@@ -1,44 +1,47 @@
-import { profile } from './view/profile.js';
-import { menu } from './view/menu.js';
-import { filmList } from './view/film-list.js';
-import { filmCard } from './view/film-card.js';
-import { showMoreButton } from './view/show-more-button.js';
-import { popup } from './view/popup.js';
+import {profile} from './view/profile.js';
+import {menu} from './view/menu.js';
+import {filmList} from './view/film-list.js';
+import {filmCard} from './view/film-card.js';
+import {showMoreButton} from './view/show-more-button.js';
+import {popup} from './view/popup.js';
+import {generateFilmCard} from './mock/film-card.js';
 
-const FILMS_IN_LIST = 5;
+const FILMS_IN_LIST = 20;
 const TOP_RATED_FILMS = 2;
 const MOST_COMMENTED_FILMS = 2;
 
+const films = new Array(FILMS_IN_LIST).fill().map(generateFilmCard);
+
 const render = (container, template, place) => {
-    container.insertAdjacentHTML(place, template);
-}
+  container.insertAdjacentHTML(place, template);
+};
 
-const header = document.querySelector('header');
-const mainContainer = document.querySelector('.main');
-const documentBody = document.querySelector('body');
-
-
-render(header, profile(), 'beforeend');
-render(mainContainer, menu(), 'afterbegin');
-render(mainContainer, filmList(), 'beforeend');
+const header = document.querySelector(`header`);
+const mainContainer = document.querySelector(`.main`);
+const documentBody = document.querySelector(`body`);
 
 
-const filmListContainer = document.querySelector('.films-list > .films-list__container');
-const topRatedContainer = document.querySelector('.films-list + .films-list--extra .films-list__container');
-const mostCommentedContainer = document.querySelector('.films-list--extra + .films-list--extra .films-list__container');
+render(header, profile(), `beforeend`);
+render(mainContainer, menu(), `afterbegin`);
+render(mainContainer, filmList(), `beforeend`);
+
+
+const filmListContainer = document.querySelector(`.films-list > .films-list__container`);
+const topRatedContainer = document.querySelector(`.films-list + .films-list--extra .films-list__container`);
+const mostCommentedContainer = document.querySelector(`.films-list--extra + .films-list--extra .films-list__container`);
 
 
 for (let i = 0; i < FILMS_IN_LIST; i++) {
-    render(filmListContainer, filmCard(), 'afterbegin');
+  render(filmListContainer, filmCard(films[i]), `afterbegin`);
 }
 
 for (let i = 0; i < TOP_RATED_FILMS; i++) {
-    render(topRatedContainer, filmCard(), 'beforeend');
+  render(topRatedContainer, filmCard(films[i]), `beforeend`);
 }
 
 for (let i = 0; i < MOST_COMMENTED_FILMS; i++) {
-    render(mostCommentedContainer, filmCard(), 'beforeend');
+  render(mostCommentedContainer, filmCard(films[i]), `beforeend`);
 }
 
-render(filmListContainer, showMoreButton(), 'afterend');
-render(documentBody, popup(), 'beforeend');
+render(filmListContainer, showMoreButton(), `afterend`);
+render(documentBody, popup(films[0]), `beforeend`);
---
+++ src/mock/film-card.js
@@ -0,0 +1,127 @@
+import {getRandomInteger} from 'utilites.js';
+import {generateRandomValue} from 'utilites.js';
+import dayjs from "dayjs";
+
+const titles = [
+  `Made for each other`,
+  `Popeye meets Sinbad`,
+  `Sagebrush trail`,
+  `Santa Claus conquers the Martians`,
+  `The dance of life`,
+  `The great flamarion`,
+  `The man with the golden arm`
+];
+
+const posters = [
+  `made-for-each-other.png`,
+  `popeye-meets-Sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
+
+const producers = [
+  `Peter Jackson`,
+  `Christofer Nolan`,
+  `Aneesh Chaganty`,
+];
+
+const screenwriters = [
+  `Quentin Tarantino`,
+  `Woody Allen`,
+  `Ethan Coen`,
+  `Aaron Sorkin`
+];
+
+const actors = [
+  `Brad Pitt`,
+  `Margot Robbie`,
+  `Joaquin Phoenix`,
+  `Tom Hardy`,
+  `Matthew McConaughey`,
+];
+
+const country = [
+  `USA`,
+  `South Korea`,
+  `France`,
+  `Canada`,
+  `Israel`,
+];
+
+const genres = [
+  `comedy`,
+  `drama`,
+  `triller`,
+  `horror`,
+  `western`,
+  `sci-fi`,
+  `fantasy`
+];
+
+const ageRatings = [`0+`, `6+`, `12+`, `16+`, `18+`];
+
+const descriptionTemplate = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Cras aliquet varius magna, non porta ligula feugiat eget.
+Fusce tristique felis at fermentum pharetra.
+Aliquam id orci ut lectus varius viverra.
+Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.
+Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.
+Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.
+Sed sed nisi sed augue convallis suscipit in sed felis.
+Aliquam erat volutpat.
+Nunc fermentum tortor ac porta dapibus.
+In rutrum ac purus sit amet tempus`;
+
+const generateDataRelease = () => {
+  const maxDaysGap = 365;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+
+  return dayjs().add(daysGap, `day`).toDate();
+};
+
+const generateDescription = () => {
+  const quantitySentences = getRandomInteger(1, 5);
+  const sentences = descriptionTemplate.split(`.`);
+  let randomDescription = ``;
+
+  for (let i = 0; i < quantitySentences; i++) {
+    randomDescription += generateRandomValue(sentences) + `. `;
+  }
+
+  return randomDescription;
+};
+
+const generateGenre = () => {
+  const quantityGenres = getRandomInteger(1, 3);
+  let randomGenres = [];
+
+  for (let i = 0; i < quantityGenres; i++) {
+    randomGenres.push(generateRandomValue(genres));
+  }
+
+  return randomGenres;
+};
+
+export const generateFilmCard = () => {
+  return {
+    title: generateRandomValue(titles),
+    originTitle: generateRandomValue(titles),
+    poster: `/images/posters/` + generateRandomValue(posters),
+    rating: (Math.random() * 10).toFixed(1),
+    producer: generateRandomValue(producers),
+    screenwriters: generateRandomValue(screenwriters),
+    actors: generateRandomValue(actors),
+    year: getRandomInteger(1895, 2020),
+    release: generateDataRelease(),
+    duration: getRandomInteger(0, 3) + `h ` + getRandomInteger(0, 59) + `m`,
+    country: generateRandomValue(country),
+    genre: generateGenre(),
+    description: generateDescription(),
+    comments: getRandomInteger(0, 5),
+    ageRating: generateRandomValue(ageRatings),
+  };
+};
+
